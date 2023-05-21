6076b92e14818cd9adfa047393ca9f6212716a33
mkvanam <mkvanam@gmail.com>
2023-05-21 08:55:04 +0530

    adding project1

---
+++ GIT_Practice_Pjt/src/SampleClass.java
@@ -0,0 +1,10 @@
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample project for GIT commit practice ");
+
+	}
+
+}
